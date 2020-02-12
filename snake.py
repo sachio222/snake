ba84f995dd77d0d0669985aeb31ca674388fbab1
sachio222 <brainpowerux@gmail.com>
2020-02-11 19:55:58 -0500

    created

---
+++ snake.py
@@ -0,0 +1,72 @@
+import curses
+import random
+
+s = curses.initscr()  # Initialize screen
+curses.curs_set(0)  # Hide cursor
+sh, sw = s.getmaxyx()  # Get height and width
+w = curses.newwin(sh, sw, 0, 0)  # Define window
+w.keypad(1)  # Enable Keyboard input
+w.timeout(127)  # Refresh rate
+
+# Define initial snake head position
+snk_x = sw // 3
+snk_y = sh // 2
+
+# Snake is a vector of coordinates
+snake = [[snk_y, snk_x], [snk_y, snk_x - 1], [snk_y, snk_x - 2]]
+
+# Food is a cordinate
+food = [sh // 2, sw // 2]
+
+# Add initial food to screen with add character
+w.addch(food[0], food[1], curses.ACS_PI)
+
+# Initialize key
+key = curses.KEY_RIGHT
+
+while True:
+    next_key = w.getch()  # Listen for keys
+
+    # If no keypress during loop (key = -1)
+    key = key if next_key == -1 else next_key
+
+    # Check if snake dead
+    if snake[0][0] in [0, sh] or snake[0][1] in [0, sw
+                                                ] or snake[0] in snake[1:]:
+        #     # End behavior
+        curses.endwin()
+        quit()
+
+    # draw
+    new_head = [snake[0][0], snake[0][1]]
+
+    # Update new head based on key press
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+
+    # draw
+    # Array operation
+    snake.insert(0, new_head)  # Puts new head at the start of the snake
+
+    # Check if snake has eaten food
+    if snake[0] == food:
+        food = None
+        while food is None:
+            # New food is placed within bounds of screen
+            nf = [random.randint(1, sh - 1), random.randint(1, sw - 1)]
+            # Place the food anywhere on the screen, unless it's in snake
+            food = nf if nf not in snake else None
+        w.addch(food[0], food[1], curses.ACS_PI)
+    else:
+        # Keep snake moving, pop off last bit
+        tail = snake.pop()
+        w.addch(tail[0], tail[1], ' ')
+
+    # Draw snake
+    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
---
+++ snake_classified.py
@@ -0,0 +1,111 @@
+import curses
+import random
+
+class Snake():
+    """Snake object"""
+
+    def __init__(self, sw, sh, w):
+        super(Snake, self).__init__()
+        # Define initial snake head position
+        self.width, self.height, self.window = sw, sh, w
+
+        self.snk_x = self.width // 3
+        self.snk_y = self.height // 2
+
+        # Snake is a vector of coordinates
+        self.snake = [[self.snk_y, self.snk_x], [self.snk_y, self.snk_x - 1],
+                      [self.snk_y, self.snk_x - 2]]
+
+    def move(self, key, food=False):
+        self._point_head(key)
+        self._check_alive()
+        self._trail(food)
+        self._draw()
+
+    def _check_alive(self):
+        if self.snake[0][0] in [0, self.height] or self.snake[0][1] in [
+                0, self.width
+        ] or self.snake[0] in self.snake[1:]:
+            curses.endwin()
+            quit()
+
+    def _point_head(self, key):
+        new_head = [self.snake[0][0], self.snake[0][1]]
+
+        # Update new head based on key press
+        if key == curses.KEY_UP:
+            new_head[0] -= 1
+        if key == curses.KEY_RIGHT:
+            new_head[1] += 1
+        if key == curses.KEY_DOWN:
+            new_head[0] += 1
+        if key == curses.KEY_LEFT:
+            new_head[1] -= 1
+
+        # Puts new head at the start of the snake
+        self.snake.insert(0, new_head)
+
+    def _trail(self, food=False):
+        if not food:
+            tail = self.snake.pop()
+            self.window.addch(tail[0], tail[1], ' ')
+
+    def _draw(self):
+        self.window.addch(self.snake[0][0], self.snake[0][1],
+                          curses.ACS_CKBOARD)
+
+    def __call__(self):
+        # Head position
+        return self.snake[0]
+
+    get_pos = __call__
+
+
+class Food():
+    """Food object"""
+
+    def __init__(self, sw, sh, w):
+        super(Food, self).__init__()
+        self.width, self.height, self.window = sw, sh, w
+
+        self.food = [self.height // 2, self.width // 2]
+        self._draw()
+
+    def put(self, snake):
+        # New food is placed within bounds of screen
+        nf = [
+            random.randint(1, self.height - 1),
+            random.randint(1, self.width - 1)
+        ]
+        self.food = nf
+        self._draw()
+
+    def _draw(self):
+        self.window.addch(self.food[0], self.food[1], curses.ACS_PI)
+
+    def __call__(self):
+        return self.food
+
+    get_pos = __call__
+
+
+s = curses.initscr()  # Initialize screen
+curses.curs_set(0)  # Hide cursor
+height, width = s.getmaxyx()  # Get height and width
+window = curses.newwin(height, width, 0, 0)  # Define window
+window.keypad(1)  # Enable Keyboard input
+window.timeout(88)  # Refresh rate
+
+snake = Snake(width, height, window)
+food = Food(width, height, window)
+# Initialize key
+key = curses.KEY_RIGHT
+
+while True:
+    next_key = window.getch()  # Listen for keys
+    key = key if next_key == -1 else next_key
+    snake.move(key)
+
+    if snake() == food():
+        snake.move(key, food=True)
+        food.put(snake())
